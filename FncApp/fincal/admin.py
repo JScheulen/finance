77d220bfd8f870cf0562caa26eda0607353be0c7
JScheulen <152833532+JScheulen@users.noreply.github.com>
2024-06-19 00:38:45 -0400

    Nuevo Repositorio

--- FncApp/fincal/admin.py
+++ FncApp/fincal/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Monedas
+from .models import Monedas, Proyects, ProyectImg
 
 # Register your models here.
 
 admin.site.register(Monedas)
+admin.site.register(Proyects)
+admin.site.register(ProyectImg)
