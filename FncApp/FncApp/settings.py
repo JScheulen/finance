9eacb84192857709fcabc5bd00bfccff9b9cba02
JScheulen <152833532+JScheulen@users.noreply.github.com>
2024-06-19 00:23:28 -0400

    Nuevo Repositorio

--- FncApp/FncApp/settings.py
+++ FncApp/FncApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nv@l_-!r-!%258nb0&@-m^3y4635(0*1b&pxk$1^+%02o_#4$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #App
+    # App
     'fincal',
     'FncApp',
 ]
--- FncApp/fincal/models.py
+++ FncApp/fincal/models.py
@@ -8,3 +8,15 @@ class Monedas(models.Model):
     moneda = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=255)
     precio = models.FloatField(blank=True, null=True)
+
+
+class Proyects(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.titulo
+
+class ProyectImg(models.Model):
+    proyecto = models.ForeignKey(Proyects, on_delete=models.CASCADE)
+    imgList = models.ImageField()
+    imgDescript = models.CharField(max_length=255)
